e79d9d74c7404a95e8874a75751e8a1822d043b9
swara12262 <swaranjalisawant@gmail.com>
2020-07-24 19:03:43 +0530

    @Componenet annotation without using bean.xml

---
+++ Spring-Common-Annotations/src/com/spring/college/Client.java
@@ -0,0 +1,15 @@
+package com.spring.college;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Client {
+
+	public static void main(String[] args) {
+		ApplicationContext context=new AnnotationConfigApplicationContext(CollegeConfig.class);
+		College college=context.getBean("collegeBean", College.class);
+		System.out.println("Object created");
+	}
+
+}
---
+++ Spring-Common-Annotations/src/com/spring/college/College.java
@@ -0,0 +1,9 @@
+package com.spring.college;
+
+import org.springframework.stereotype.Component;
+
+@Component("collegeBean")
+public class College {
+	
+
+}
